c3b1222bb21814757f354341dd36fb6e9ceb3e0e
Ilya <selivanov.ilya@gmail.com>
2021-08-18 08:54:32 +0300

    console app

---
+++ src/CutMp3.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System.IO;
+using VideoLibrary;
+
+namespace CutMp3.UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = @"C:\Users\Ilya\Downloads\";
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo("https://www.youtube.com/watch?v=26zPA3mA7mk");
+            File.WriteAllBytes(source + vid.FullName, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = source + vid.FullName };
+            var outputFile = new MediaFile { Filename = $"{source + vid.FullName}.mp3" };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
+        }
+    }
+}
